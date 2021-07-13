44a8e1664f981f0bf8b817c1955d684865f93780
koval2001 <dianakoval2001@icloud.com>
2021-07-13 18:39:29 +0300

    modify: from fahrenheit to celcius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,14 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=f75501c6933d9ea3acc21dad478fcbe8&query=' + latitude + ',' + longitude +'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=f75501c6933d9ea3acc21dad478fcbe8&query=' + latitude + ',' + longitude
     request({ url, json:true}, (error, { body }) => {
         if(error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.error) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees Fahrenheit out. There is a ' + body.current.precip + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
